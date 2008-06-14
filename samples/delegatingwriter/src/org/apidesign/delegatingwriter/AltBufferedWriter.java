16bbbcc6f3c6331622b10a5f08d16082168b47f5
Jaroslav Tulach <jtulach@netbeans.org>
2008-06-14 09:53:03 +0200

    Demo for writer throwing an exception; and also writing out CD content

---
+++ samples/delegatingwriter/src/org/apidesign/delegatingwriter/AltBufferedWriter.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.apidesign.delegatingwriter;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * This is a regular {@link BufferedWriter}, just its implementation
+ * of the append method can choose from three options. This allows us to
+ * simulate the potential pros and cons of various possible implementations.
+ * 
+ * @author Jaroslav Tulach <jaroslav.tulach@apidesign.org>
+ */
+public class AltBufferedWriter extends BufferedWriter {
+    private final Writer out;
+    private final Behaviour behaviour;
+    
+    public AltBufferedWriter(Writer out, Behaviour behaviour) {
+        super(out);
+        this.out = out;
+        this.behaviour = behaviour;
+    }
+    
+    @Override
+    public Writer append(CharSequence csq) throws IOException {
+        switch (behaviour) {
+            case THROW_EXCEPTION: return appendThrowException(csq); 
+            case DELEGATE_TO_SUPER: return appendDelegateToSuper(csq);
+            case DELEGATE_TO_OUT: return appendDelegateToUnderlaying(csq);
+            case DELEGATE_CONDITIONALLY: return appendDelegateConditionally(csq);
+            default: throw new IllegalStateException("Unknown" + behaviour);
+        }
+    }
+    
+    // BEGIN: writer.throw
+    public Writer appendThrowException(CharSequence csq) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+    // END: writer.throw
+    
+    public Writer appendDelegateToSuper(CharSequence csq) throws IOException {
+        // non-efficient variant of delegating via converting to String first 
+        // and using one of methods that existed in 1.4
+        // BEGIN: writer.throw.client
+        if (csq == null) {
+            write("null");
+        } else {
+            write(csq.toString());
+        }
+        return this;
+        // END: writer.throw.client
+    }
+    
+    public Writer appendDelegateToUnderlaying(CharSequence csq) throws IOException {
+        // BEGIN: writer.delegateout
+        // efficient, yet dangerous delegation skipping methods known to 
+        // subclasses that used version 1.4
+        out.append(csq);
+        return this;
+        // END: writer.delegateout
+    }
+
+    private Writer appendDelegateConditionally(CharSequence csq) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+    public enum Behaviour {
+        THROW_EXCEPTION, DELEGATE_TO_SUPER, DELEGATE_TO_OUT, DELEGATE_CONDITIONALLY
+    }
+}
---
+++ samples/delegatingwriter/test/org/apidesign/delegatingwriter/CountingWriter.java
@@ -0,0 +1,72 @@
+
+package org.apidesign.delegatingwriter;
+
+import java.io.IOException;
+import java.io.Writer;
+
+// BEGIN: writer.CountingWriter
+/** Writer that counts the number of written in characters.
+ *
+ * @author Jaroslav Tulach
+ */
+public class CountingWriter extends Writer {
+    private int counter;
+    
+    
+    public int getCharacterCount() {
+        return counter;
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        counter += len;
+    }
+
+    @Override
+    public Writer append(CharSequence csq) throws IOException {
+        counter += csq.length();
+        return this;
+    }
+// END: writer.CountingWriter
+
+    @Override
+    public Writer append(CharSequence csq, int start, int end) throws IOException {
+        counter += (end - start);
+        return this;
+    }
+
+    @Override
+    public Writer append(char c) throws IOException {
+        counter++;
+        return this;
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        counter++;
+    }
+
+    @Override
+    public void write(char[] cbuf) throws IOException {
+        counter += cbuf.length;
+    }
+
+    @Override
+    public void write(String str) throws IOException {
+        counter += str.length();
+    }
+
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        counter += len;
+    }
+
+    @Override
+    public void flush() throws IOException {
+    }
+
+    @Override
+    public void close() throws IOException {
+    }
+
+}
