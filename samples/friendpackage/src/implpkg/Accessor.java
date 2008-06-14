9708b65f1bfc5336f084efaa157df7c27b683f91
Jaroslav Tulach <jtulach@netbeans.org>
2008-06-14 09:55:09 +0200

    part 2 of polishing "review2" areas

--- samples/friendpackage/src/implpkg/Accessor.java
+++ samples/friendpackage/src/implpkg/Accessor.java
@@ -22,27 +22,13 @@ import javax.swing.event.ChangeListener;
  */
 // BEGIN: design.less.friend.Accessor
 public abstract class Accessor {
-    private static volatile Accessor DEFAULT;
-    public static Accessor getDefault() {
-        Accessor a = DEFAULT;
-        if (a != null) {
-            return a;
-        }
-        
+    public static Accessor DEFAULT;
+    static {
         try {
             Class.forName(Item.class.getName(), true, Item.class.getClassLoader());
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        
-        return DEFAULT;
-    }
-
-    public static void setDefault(Accessor accessor) {
-        if (DEFAULT != null) {
-            throw new IllegalStateException();
-        }
-        DEFAULT = accessor;
     }
     
     public Accessor() {
