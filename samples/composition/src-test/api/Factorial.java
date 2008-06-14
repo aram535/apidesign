6d8be98999408996ec230488ee963363f4700e9d
Jaroslav Tulach <jtulach@netbeans.org>
2008-06-14 10:04:45 +0200

    Splitting Factorial into own class, so Andrei does not need to complain about it having static modifier

---
+++ samples/composition/src-test/api/Factorial.java
@@ -0,0 +1,18 @@
+package api;
+
+/** Class showing inventive, non-expected use of 
+ * Arithmetica methods to do multiplication instead of
+ * addition.
+ */
+//BEGIN: design.composition.arith.factorial
+public final class Factorial extends Arithmetica {
+    public static int factorial(int n) {
+        return new Factorial().sumRange(1, n);
+    }
+    @Override
+    public int sumTwo(int one, int second) {
+        return one * second;
+    }
+}
+//END: design.composition.arith.factorial
+
