b2567e5845e7a9a603e3a595550be30e520a069b
Jaroslav Tulach <jtulach@netbeans.org>
2008-06-14 10:03:34 +0200

    Jesse wanted to have better example of InstanceProvider. Done, but it is also more complicated: http://wiki.apidesign.org/index.php/A_Method_Addition_Lover's_Heaven

--- samples/instanceof/src/api/InstanceProvider.java
+++ samples/instanceof/src/api/InstanceProvider.java
@@ -3,7 +3,7 @@ package api;
 
 // BEGIN: instanceof.InstanceProvider
 public interface InstanceProvider {
-    public Class<?> instanceClass();
-    public Object instanceCreate();
+    public Class<?> instanceClass() throws Exception;
+    public Object instanceCreate() throws Exception;
 }
 // END: instanceof.InstanceProvider
--- samples/instanceofclass/src-api2.0/api/InstanceProvider.java
+++ samples/instanceofclass/src-api2.0/api/InstanceProvider.java
@@ -2,24 +2,50 @@
 package api;
 
 // BEGIN: instanceof.class.InstanceProvider2
-public final class InstanceProvider {
-    private final Object instance;
 
-    public InstanceProvider(Object instance) {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Callable;
+
+public final class InstanceProvider {
+    private final Callable<Object> instance;
+    private final Set<String> types;
+
+    public InstanceProvider(Callable<Object> instance) {
         this.instance = instance;
+        this.types = null;
+    }
+    /** Specifies not only a factory for creating objects, but
+     * also additional information about them.
+     * @param instance the factory to create the object
+     * @param type the class that the create object will be instance of
+     * @since 2.0 
+     */
+    public InstanceProvider(Callable<Object> instance, String... types) {
+        this.instance = instance;
+        this.types = new HashSet<String>();
+        this.types.addAll(Arrays.asList(types));
     }
     
-    public Class<?> instanceClass() {
-        return instance.getClass();
+    public Class<?> instanceClass() throws Exception {
+        return instance.call().getClass();
     }
-    public Object instanceCreate() {
-        return instance;
+    public Object instanceCreate() throws Exception {
+        return instance.call();
     }
     
-    /** @since 2.0 */
-    public boolean isInstanceOf(Class<?> c) {
-        if (Helper.knownHowToDoItBetter()) {
-            return Helper.computeTheResultOfIsInstanceOfInSomeBetterWay(c);
+    /** Allows to find out if the InstanceProvider creates object of given
+     * type. This check can be done without loading the actual object or
+     * its implementation class into memory.
+     * 
+     * @param c class to test 
+     * @return if the instances produced by this provider is instance of c
+     * @since 2.0 
+     */
+    public boolean isInstanceOf(Class<?> c) throws Exception {
+        if (types != null) {
+            return types.contains(c.getName());
         } else {
             // fallback
             return c.isAssignableFrom(instanceClass());
@@ -29,12 +55,3 @@ public final class InstanceProvider {
     
 }
 // END: instanceof.class.InstanceProvider2
-
-class Helper {
-    static boolean computeTheResultOfIsInstanceOfInSomeBetterWay(Class<?> c) {
-        return false;
-    }
-    static boolean knownHowToDoItBetter() {
-        return false;
-    }
-}
