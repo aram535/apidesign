5fe6b6e266683b48d33e0f3b048b1ae557a54283
Jaroslav Tulach <jtulach@netbeans.org>
2008-06-14 09:54:28 +0200

    Onward! Towards deadlock tests.

---
+++ samples/deadlock/src/org/apidesign/deadlock/LabelProvider.java
@@ -0,0 +1,24 @@
+package org.apidesign.deadlock;
+
+import java.util.HashSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class LabelProvider extends JPanel {
+    private static Logger LOG = Logger.getLogger(LabelProvider.class.getName());
+    
+    // BEGIN: deadlock.provider
+    private HashSet<JLabel> allCreated = new HashSet<JLabel>();
+
+    public synchronized JLabel createLabel () {
+        LOG.log(Level.INFO, "Will create JLabel");
+        JLabel l = new JLabel ();
+        LOG.log(Level.INFO, "Label created {0}", l);
+        allCreated.add (l);
+        return l;
+    }
+    // END: deadlock.provider
+
+}
