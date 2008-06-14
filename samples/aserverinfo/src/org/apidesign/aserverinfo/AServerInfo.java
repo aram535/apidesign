acd73ff0bc128eabdf0bc03bf61258ccfe194b5d
Jaroslav Tulach <jtulach@netbeans.org>
2008-06-14 09:53:57 +0200

    Way simplified internals of the AServerInfo class

--- samples/aserverinfo/src/org/apidesign/aserverinfo/AServerInfo.java
+++ samples/aserverinfo/src/org/apidesign/aserverinfo/AServerInfo.java
@@ -1,43 +1,43 @@
 package org.apidesign.aserverinfo;
 
 import java.net.URL;
-import java.util.concurrent.Callable;
 import org.openide.util.Lookup;
 
 public final class AServerInfo {
-    private final Callable<String> name;
-    private final Callable<URL> url;
-    private final Callable<Void> reset;
-    private final Callable<Void> shutdown;
+    private final NameProvider name;
+    private final URLProvider url;
+    private final ResetHandler reset;
+    private final ShutdownHandler shutdown;
 
-    private AServerInfo(Callable<String> name, Callable<URL> url, Callable<Void> reset, Callable<Void> s) {
+    private AServerInfo(NameProvider name, URLProvider url, ResetHandler reset, ShutdownHandler shutdown) {
         this.name = name;
         this.url = url;
         this.reset = reset;
-        this.shutdown = s;
+        this.shutdown = shutdown;
     }
+
     
     //
     // API for clients
     //
     
     public String getName() {
-        return call(name, "noname");
+        return name == null ? "noname" : name.getName();
     }
     
     public URL getURL() {
-        return call(url, null);
+        return url == null ? null : url.getURL();
     }
     
-    public void reset() throws Exception {
+    public void reset() {
         if (reset != null) {
-            reset.call();
+            reset.reset();
         }
     }
     
-    public void shutdown() throws Exception {
+    public void shutdown() {
         if (shutdown != null) {
-            shutdown.call();
+            shutdown.shutdown();
         }
     }
 
@@ -59,25 +59,24 @@ public final class AServerInfo {
     public static AServerInfo create(final Lookup interfaces) 
     // END: aserverinfo.create
     {
-        Callable<String> nameP = new Callable<String>() {
-            public String call() throws Exception {
+        NameProvider nameP = new NameProvider() {
+            public String getName() {
                 NameProvider p = interfaces.lookup(NameProvider.class);
                 return p == null ? "noname" : p.getName();
             }
         };
-        Callable<URL> urlP = new Callable<URL>() {
-            public URL call() throws Exception {
+        URLProvider urlP = new URLProvider() {
+            public URL getURL() {
                 URLProvider p = interfaces.lookup(URLProvider.class);
                 return p == null ? null : p.getURL();
             }
         };
-        Callable<Void> resetP = new Callable<Void>() {
-            public Void call() throws Exception {
+        ResetHandler resetP = new ResetHandler() {
+            public void reset() {
                 ResetHandler h = interfaces.lookup(ResetHandler.class);
                 if (h != null) {
                     h.reset();
                 }
-                return null;
             }
         };
         
@@ -88,7 +87,7 @@ public final class AServerInfo {
     public static AServerInfo create(NameProvider nameProvider, URLProvider urlProvider, ResetHandler reset)
     // END: aserverinfo.regularcreate
     {
-        return create(nameProvider, urlProvider, reset, null);
+        return new AServerInfo(nameProvider, urlProvider, reset, null);
     }
     
     // BEGIN: aserverinfo.regularcreate.withshutdown
@@ -101,39 +100,7 @@ public final class AServerInfo {
     public static AServerInfo create(NameProvider nameProvider, URLProvider urlProvider, ResetHandler reset, ShutdownHandler shutdown)
     // END: aserverinfo.regularcreate.withshutdown
     {
-        final NameProvider np = nameProvider;
-        final URLProvider up = urlProvider;
-        final ResetHandler h = reset;
-        final ShutdownHandler s = shutdown;
-        
-        Callable<String> nameP = new Callable<String>() {
-            public String call() throws Exception {
-                return np == null ? "noname" : np.getName();
-            }
-        };
-        Callable<URL> urlP = new Callable<URL>() {
-            public URL call() throws Exception {
-                return up == null ? null : up.getURL();
-            }
-        };
-        Callable<Void> resetP = new Callable<Void>() {
-            public Void call() throws Exception {
-                if (h != null) {
-                    h.reset();
-                }
-                return null;
-            }
-        };
-        Callable<Void> shutP = new Callable<Void>() {
-            public Void call() throws Exception {
-                if (s != null) {
-                    s.shutdown();
-                }
-                return null;
-            }
-        };
-        
-        return new AServerInfo(nameP, urlP, resetP, shutP);
+        return new AServerInfo(nameProvider, urlProvider, reset, shutdown);
     }
 
     //
@@ -146,57 +113,17 @@ public final class AServerInfo {
     }
     
     public final AServerInfo nameProvider(final NameProvider np) {
-        Callable<String> nameP = new Callable<String>() {
-            public String call() throws Exception {
-                return np == null ? "noname" : np.getName();
-            }
-        };
-        return new AServerInfo(nameP, this.url, this.reset, this.shutdown);
+        return new AServerInfo(np, this.url, this.reset, this.shutdown);
     }
     // END: aserverinfo.cumulative.empty
 
     public final AServerInfo urlProvider(final URLProvider up) {
-        Callable<URL> urlP = new Callable<URL>() {
-            public URL call() throws Exception {
-                return up == null ? null : up.getURL();
-            }
-        };
-        return new AServerInfo(this.name, urlP, this.reset, this.shutdown);
+        return new AServerInfo(this.name, up, this.reset, this.shutdown);
     }
     public final AServerInfo reset(final ResetHandler h) {
-        Callable<Void> resetP = new Callable<Void>() {
-            public Void call() throws Exception {
-                if (h != null) {
-                    h.reset();
-                }
-                return null;
-            }
-        };
-        return new AServerInfo(this.name, this.url, resetP, this.shutdown);
+        return new AServerInfo(this.name, this.url, h, this.shutdown);
     }
     public final AServerInfo shutdown(final ShutdownHandler s) {
-        Callable<Void> shutP = new Callable<Void>() {
-            public Void call() throws Exception {
-                if (s != null) {
-                    s.shutdown();
-                }
-                return null;
-            }
-        };
-        return new AServerInfo(this.name, this.url, this.reset, shutP);
+        return new AServerInfo(this.name, this.url, this.reset, s);
     }
-    
-    //
-    // Support implementations
-    //
-    
-    private static <T> T call(Callable<T> name, T defValue) {
-        try {
-            return name.call();
-        } catch (Exception ex) {
-            return defValue;
-        }
-    }
-
-    
 }
