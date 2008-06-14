3bc82797e45449be614bc9bc46ed5abcb0d2ab9b
Jaroslav Tulach <jtulach@netbeans.org>
2008-06-14 10:04:06 +0200

    Reimplementing the lookups once again

--- samples/consistency/src-api2.0/api/Lookup.java
+++ samples/consistency/src-api2.0/api/Lookup.java
@@ -1,6 +1,11 @@
 package api;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 /** Simplified version of NetBeans 
@@ -13,26 +18,48 @@ import java.util.Set;
  * @version 2.0
  */
 // BEGIN: design.consistency.2.0
-public final class Lookup {
+public abstract class Lookup {
+    /** only for classes in the same package */
     Lookup() {
     }
     
     // BEGIN: design.consistency.lookup.2.0
     public <T> T lookup(Class<T> clazz) {
-        return null;
+        Iterator<T> it = doLookup(clazz);
+        return it.hasNext() ? it.next() : null;
     }
     // END: design.consistency.lookup.2.0
 
     // BEGIN: design.consistency.lookupAll.2.0
     public <T> Collection<? extends T> lookupAll(Class<T> clazz) {
-        return null;
+        Iterator<T> it = doLookup(clazz);
+        if (!it.hasNext()) {
+            return Collections.emptyList();
+        } else {
+            List<T> result = new ArrayList<T>();
+            while (it.hasNext()) {
+                result.add(it.next());
+            }
+            return result;
+        }
     }
     // END: design.consistency.lookupAll.2.0
 
     // BEGIN: design.consistency.lookupAllClasses.2.0
     public <T> Set<Class<? extends T>> lookupAllClasses(Class<T> clazz) {
-        return null;
+        Iterator<T> it = doLookup(clazz);
+        if (!it.hasNext()) {
+            return Collections.emptySet();
+        } else {
+            Set<Class<? extends T>> result = new HashSet<Class<? extends T>>();
+            while (it.hasNext()) {
+                result.add(it.next().getClass().asSubclass(clazz));
+            }
+            return result;
+        }
     }
     // END: design.consistency.lookupAllClasses.2.0
+// FINISH: design.consistency.2.0
+    
+    abstract <T> Iterator<T> doLookup(Class<T> clazz);
 }
-// END: design.consistency.2.0
--- samples/consistency/src-api2.0/api/Lookups.java
+++ samples/consistency/src-api2.0/api/Lookups.java
@@ -1,6 +1,8 @@
 package api;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.Set;
 
 /** Factory to create various types of lookup instances.
@@ -12,12 +14,30 @@ public final class Lookups {
     private Lookups() {
     }
     
-    public static Lookup fixed(Object... instances) {
-        return null;
+    public static Lookup fixed(final Object... instances) {
+        return new Lookup() {
+            @Override
+            <T> Iterator<T> doLookup(Class<T> clazz) {
+                ArrayList<T> result = new ArrayList<T>();
+                for (Object obj : instances) {
+                    if (clazz.isInstance(obj)) {
+                        result.add(clazz.cast(obj));
+                    }
+                }
+                return result.iterator();
+            }
+        };
     }
     
-    public static Lookup dynamic(Dynamic provider) {
-        return null;
+    public static Lookup dynamic(final Dynamic provider) {
+        return new Lookup() {
+            @Override
+            <T> Iterator<T> doLookup(Class<T> clazz) {
+                ArrayList<T> result = new ArrayList<T>();
+                provider.computeInstances(clazz, result);
+                return result.iterator();
+            }
+        };
     }
     
     public interface Dynamic {
