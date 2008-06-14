476f1ee91d25c2df1bdf8d463c60b5731ef8103b
Jaroslav Tulach <jtulach@netbeans.org>
2008-06-14 09:58:35 +0200

    default traversal described

---
+++ samples/visitor/traversal/src-test/org/apidesign/test/visitor/LanguageCheckTest.java
@@ -0,0 +1,51 @@
+package org.apidesign.test.visitor;
+
+import static junit.framework.Assert.*;
+import org.apidesign.visitor.Language.Expression;
+import org.apidesign.visitor.Language.Number;
+import org.apidesign.visitor.Language.Plus;
+import org.apidesign.visitor.Language.Visitor;
+import org.junit.Test;
+
+public class LanguageCheckTest {
+
+    // BEGIN: visitor.language.check.visitunknown
+    private static class Valid1_0Language extends Visitor/*version1.0*/ {
+        boolean invalid;
+
+        @Override
+        public boolean visitUnknown(Expression exp) {
+            invalid = true;
+            return false;
+        }
+        public void visitPlus(Plus s) {
+            s.getFirst().visit(this);
+            s.getSecond().visit(this);
+        }
+        public void visitNumber(Number n) { 
+        }
+    }
+
+    public static boolean isValid1_0Language(Expression expression) {
+        Valid1_0Language valid = new Valid1_0Language();
+        expression.visit(valid);
+        return !valid.invalid;
+    }
+    // END: visitor.language.check.visitunknown
+    
+    @Test public void printOnePlusOne() {
+        Number one = new Number(1);
+        Expression expression = new Plus(one, one);
+
+        assertTrue("Valid language", isValid1_0Language(expression));
+    }
+
+    @Test public void printOnePlusTwoPlusThree() {
+        Number one = new Number(1);
+        Number two = new Number(2);
+        Number three = new Number(3);
+        Expression plus = new Plus(one, new Plus(two, three));
+        
+        assertTrue("Valid language", isValid1_0Language(plus));
+    }
+}
