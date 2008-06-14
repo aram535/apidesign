bef8d9fa8961718f3fccb6ef7470416356682528
Jaroslav Tulach <jtulach@netbeans.org>
2008-06-14 09:50:51 +0200

    Example with bytecode patching

---
+++ samples/differentreturntype/src-api2.0/api/API.java
@@ -0,0 +1,11 @@
+package api;
+
+import javax.swing.ImageIcon;
+
+public abstract class API {
+// BEGIN: theory.binary.differentreturntype.api2
+    public static ImageIcon getIcon() {
+// END: theory.binary.differentreturntype.api2
+        return null;
+    }
+}
