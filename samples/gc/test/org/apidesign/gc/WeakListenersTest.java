3c738f2b3b590e9d141ca160343c599ce85bb65f
Jaroslav Tulach <jtulach@netbeans.org>
2008-08-12 13:47:15 +0200

    Additional catch for OOME

--- samples/gc/test/org/apidesign/gc/WeakListenersTest.java
+++ samples/gc/test/org/apidesign/gc/WeakListenersTest.java
@@ -48,6 +48,11 @@ public class WeakListenersTest {
             ex.printStackTrace();
             // of course it cannot be garbage collected. That is OK.
             return;
+        } catch (OutOfMemoryError ex) {
+            ex.printStackTrace();
+            // sometimes the test fails on OOME, let's catch it
+            // of course it cannot be garbage collected. That is OK.
+            return;
         }
         fail("The listener cannot be GCed as it is held from long living JavaBean");
     }
