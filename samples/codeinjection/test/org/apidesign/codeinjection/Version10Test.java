c11b600f4185394d02aa7fb79eb1275a57f1a730
Jaroslav Tulach <jtulach@netbeans.org>
2009-01-10 21:29:22 +0100

    Initial version of sample to explain 'code injection'

---
+++ samples/codeinjection/test/org/apidesign/codeinjection/Version10Test.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.apidesign.codeinjection;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/** Final count down test.
+ *
+ * @author Jaroslav Tulach <jtulach@netbeans.org>
+ */
+public class Version10Test {
+
+    public Version10Test() {
+    }
+
+    /** creates version 1.0 */
+    private static CountDown create(int value) {
+        return new CountDownImplV1(value);
+    }
+
+    @Test
+    public void testDecrementFourTimes() {
+        CountDown counter = create(4);
+        assertFalse("Not down yet", counter.isDown()); counter.down();
+        assertFalse("Not down yet", counter.isDown()); counter.down();
+        assertFalse("Not down yet", counter.isDown()); counter.down();
+        assertFalse("Not down yet", counter.isDown()); counter.down();
+        assertTrue("Down now", counter.isDown());
+    }
+}
