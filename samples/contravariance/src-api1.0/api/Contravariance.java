ae54ccfd76fea98a3e9687446fae0cd9293a31b1
Jaroslav Tulach <jtulach@netbeans.org>
2011-10-18 06:55:12 +0200

    A contravariance example

---
+++ samples/contravariance/src-api1.0/api/Contravariance.java
@@ -0,0 +1,12 @@
+package api;
+
+public class Contravariance {
+    private Contravariance() {
+    }
+
+    // BEGIN: variance.contravariance.v1
+    public static boolean isPositive(Integer n1) {
+        return n1 > 0;
+    }
+    // END: variance.contravariance.v1
+}
---
+++ samples/contravariance/src-impl/test/ContravarianceTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import api.Contravariance;
+
+public class ContravarianceTest {
+    // BEGIN: variance.contravariance.test
+    public static void main(String[] args) {
+        boolean positive = Contravariance.isPositive(10);
+        boolean negative = Contravariance.isPositive(-5);
+        System.err.println("positive = " + positive + " negative = " + negative);
+        assert positive : "10 is positive";
+        assert !negative : "-5 is negative";
+    }
+    // END: variance.contravariance.test
+}
