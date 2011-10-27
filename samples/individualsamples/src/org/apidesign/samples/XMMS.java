58bf81089281d9ebd6ea5ce6e3c6926e5550bb50
Jaroslav Tulach <jtulach@netbeans.org>
2011-10-27 20:12:14 +0200

    Adding an example with defining a volume

--- samples/individualsamples/src/org/apidesign/samples/XMMS.java
+++ samples/individualsamples/src/org/apidesign/samples/XMMS.java
@@ -1,17 +1,28 @@
 package org.apidesign.samples;
 
+// BEGIN: xmms.java.api.ext
 // BEGIN: xmms.java.api
-public class XMMS {
+public final class XMMS {
     public void play() { doPlay(); }
     public void pause() { doPause(); }
     public void addToPlaylist(String file) { doAddToPlaylist(file); }
 // FINISH: xmms.java.api
-
+    /** @since 2.0 we can also control volume */
+    public void setVolume(int volume) { doTuneVolume(volume); }
+// FINISH: xmms.java.api.ext
+    
+    // BEGIN: xmms.java.spi.ext
     // BEGIN: xmms.java.spi
     interface Playback {
         public void playback(byte[] data);
+    // FINISH: xmms.java.spi
+        /** @since 2.0 we can also control volume */
+        interface Volume extends Playback {
+            public void setVolume(int volume);
+        }
     }
-    // END: xmms.java.spi
+    // END: xmms.java.spi.ext
+    
     public void registerPlayback(Playback callback) {
         this.callback = callback;
     }
@@ -26,6 +37,14 @@ public class XMMS {
     private void doPlay() {
         callback.playback("Play!".getBytes());
     }
+    // BEGIN: xmms.java.spi.instanceof
+    private void doTuneVolume(int volume) {
+        if (callback instanceof Playback.Volume) {
+            Playback.Volume pv = (Playback.Volume) callback;
+            pv.setVolume(volume);
+        }
+    }
+    // END: xmms.java.spi.instanceof
     
     
     
