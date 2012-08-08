67951235ea5c4294806f26af6f793f97840d57b5
Jaroslav Tulach <jtulach@netbeans.org>
2012-08-08 22:26:30 +0200

    Andrei: Don't put the code into main method

--- samples/erasure/src-impl/test/ErasureTest.java
+++ samples/erasure/src-impl/test/ErasureTest.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ErasureTest {
-    // BEGIN: variance.erasure.test
     public static void main(String[] args) {
+        // BEGIN: variance.erasure.test
         List<Integer> oneToTen = Arrays.asList(2, 4, 6, 8, 10);
         boolean positive = arePositive(oneToTen);
         System.err.println("positive = " + positive);
         assert positive : "All the numbers are positive: " + oneToTen;
+        // END: variance.erasure.test
     }
-    // END: variance.erasure.test
 }
