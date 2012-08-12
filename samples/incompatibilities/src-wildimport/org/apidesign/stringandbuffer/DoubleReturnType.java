43974fa49f6b08f71d4ee19141f0190b51c261ca
Jaroslav Tulach <jtulach@netbeans.org>
2012-08-12 14:14:44 +0200

    Use indentation four

--- samples/incompatibilities/src-wildimport/org/apidesign/stringandbuffer/DoubleReturnType.java
+++ samples/incompatibilities/src-wildimport/org/apidesign/stringandbuffer/DoubleReturnType.java
@@ -3,7 +3,7 @@ package org.apidesign.stringandbuffer;
 // BEGIN: theory.stringandbuffer
 /** This is not Java code, but it is fine inside the bytecode */
 public abstract class DoubleReturnType {
-  public abstract String getName(int x);
-  public abstract StringBuffer getName(int x);
+    public abstract String getName(int x);
+    public abstract StringBuffer getName(int x);
 }
 // END: theory.stringandbuffer
