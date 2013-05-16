a9b870a0d49225150ec96bc1dfce7935c49a2080
Jaroslav Tulach <jtulach@netbeans.org>
2013-05-16 08:18:24 +0200

    Yoshiki Shibata pointed out that collections were introduced in JDK 1.2

--- samples/incompatibilities/src-wildimport/org/apidesign/wildimport/MyPanel.java
+++ samples/incompatibilities/src-wildimport/org/apidesign/wildimport/MyPanel.java
@@ -1,10 +1,10 @@
-package org.apidesign.wildimport;
+ package org.apidesign.wildimport;
 
 // BEGIN: theory.wildimport
 import java.awt.*;
 import java.util.*;
 
-/** Could be compiled on JDK 1.2, before java.util.List was created */
+/** Could be compiled on JDK 1.1, before java.util.List was created */
 public class MyPanel extends Panel {
     List visualComponent = new List();
     Vector items = new Vector();
